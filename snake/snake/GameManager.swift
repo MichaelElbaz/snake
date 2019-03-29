b902f440cab790f8b8a5d0ae1ba309c7d6281532
MichaelElbaz <michael.elbaz25@gmail.com>
2019-03-29 17:05:08 -0400

    set up the home screen

---
+++ snake/snake/GameManager.swift
@@ -0,0 +1,15 @@
+//
+//  GameManager.swift
+//  snake
+//
+//  Created by Michael Elbaz on 3/10/19.
+//  Copyright © 2019 Michael Elbaz. All rights reserved.
+//
+
+import SpriteKit
+
+class GameManager {
+
+    
+    
+}
---
+++ snake/snake/GameScene.swift
@@ -0,0 +1,79 @@
+//
+//  GameScene.swift
+//  snake
+//
+//  Created by Michael Elbaz on 3/10/19.
+//  Copyright © 2019 Michael Elbaz. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    var gameLogo: SKLabelNode!
+    var bestScore: SKLabelNode!
+    var playButton: SKShapeNode!
+    var game: GameManager!
+    
+    
+    override func didMove(to view: SKView) {
+        initializeMenu()
+        game = GameManager()
+
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let touchedNode = self.nodes(at: location)
+            for node in touchedNode {
+                if node.name == "play_button" {
+                    startGame()
+                }
+            }
+        }
+    }
+    
+    private func startGame() {
+        print("start game")
+    }
+    
+    private func initializeMenu() {
+        gameLogo = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        gameLogo.zPosition = 1
+        gameLogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 200)
+        gameLogo.fontSize = 60
+        gameLogo.text = "Snake"
+        gameLogo.fontColor = SKColor.red
+        self.addChild(gameLogo)
+        //Create best score label
+        bestScore = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        bestScore.zPosition = 1
+        bestScore.position = CGPoint(x: 0, y: gameLogo.position.y - 50)
+        bestScore.fontSize = 40
+        bestScore.text = "Best Score: 0"
+        bestScore.fontColor = SKColor.white
+        self.addChild(bestScore)
+        //Create play button
+        playButton = SKShapeNode()
+        playButton.name = "play_button"
+        playButton.zPosition = 1
+        playButton.position = CGPoint(x: 0, y: (frame.size.height / -2) + 200)
+        playButton.fillColor = SKColor.cyan
+        let topCorner = CGPoint(x: -50, y: 50)
+        let bottomCorner = CGPoint(x: -50, y: -50)
+        let middle = CGPoint(x: 50, y: 0)
+        let path = CGMutablePath()
+        path.addLine(to: topCorner)
+        path.addLines(between: [topCorner, bottomCorner, middle])
+        playButton.path = path
+        self.addChild(playButton)
+    }
+
+}
